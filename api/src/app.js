2e235ba0585b26e89fc21a11bfa8e1a4bd54d484
Pete Shepley <peter.shepley@gmail.com>
2021-11-07 16:02:53 -0500

    Move to a non-serverless script

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const { MovieData } = require('./movie-data');
+const { MovieData } = require("./movie-data");
 
 const app = express();
 const movieData = new MovieData();
@@ -9,8 +9,8 @@ const movieData = new MovieData();
 app.use(cors());
 app.use(express.json());
 
-app.get('/status', async (req, res) => {
-  return res.json({status: 'OK'});
+app.get("/status", async (req, res) => {
+  return res.json({ status: "OK" });
 });
 
 function createMovieListResponse(response, config) {
@@ -20,36 +20,38 @@ function createMovieListResponse(response, config) {
       return {
         id: movie.id,
         title: movie.title,
-        posterImage: `${config.images.secure_base_url}${config.images.poster_sizes[2]}${movie.poster_path}`
+        posterImage: `${config.images.secure_base_url}${config.images.poster_sizes[2]}${movie.poster_path}`,
       };
-    })
+    }),
   };
 }
 
-app.get('/popular', async (req, res) => {
+app.get("/popular", async (req, res) => {
   const config = await movieData.getConfig();
   const page = req.query.page ? req.query.page : 1;
   const response = await movieData.getPopularList(page);
   return res.json(createMovieListResponse(response, config));
 });
 
-app.post('/search', async (req, res) => {
+app.post("/search", async (req, res) => {
   const config = await movieData.getConfig();
   const page = req.query.page ? req.query.page : 1;
   const response = await movieData.search(req.body.query, page);
   return res.json(createMovieListResponse(response, config));
 });
 
-app.get('/next', async (req, res) => {
+app.get("/next", async (req, res) => {
   // TODO: Create a next token service for fetching the next page of a movie list request.
-  return res.status(501).json({message: 'Next Token Service Not Implemented'});
+  return res
+    .status(501)
+    .json({ message: "Next Token Service Not Implemented" });
 });
 
-app.get('/movie/:id', async (req, res) => {
+app.get("/movie/:id", async (req, res) => {
   const config = await movieData.getConfig();
   const response = await movieData.getMovieDetails(req.params.id);
   response.poster_path = `${config.images.secure_base_url}original${response.poster_path}`;
   return res.json(response);
 });
 
-module.exports = app;
+app.listen(8080);
--- api/src/lambda.js
+++
@@ -1,9 +0,0 @@
-const slsExpress = require('aws-serverless-express');
-const app = require('./app');
-
-async function handler(event, context) {
-  const server = slsExpress.createServer(app);
-  return slsExpress.proxy(server, event, context, 'PROMISE').promise;
-}
-
-module.exports.handler = handler;
